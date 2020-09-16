96087045adc104aac83cf3f269ac630bfdad220b
Hao Hoang <38025803+haohoangofficial@users.noreply.github.com>
2020-09-17 00:04:39 +0700

    Update facebook.js

--- facebook.js
+++ facebook.js
@@ -41,16 +41,10 @@ function Facebook(accessToken, groupId)
         return response.id;
     };
 
-    this.postNews = async(images) => {
+    this.postNews = async(images,message) => {
         let formPublish = new FormData();
         let date = moment().format('dddd').toUpperCase() + ' NGÀY '+ moment().format('LL').toUpperCase();
-        formPublish.append('message',
-            `TIN TỨC BUỔI TRƯA ${date}\n` +
-            '\n' +
-            'Thông tin\n' +
-            '* Bài viết được cập nhật vào lúc 12 giờ trưa hàng ngày\n' +
-            '* Theo dõi bảng tin trên Instagram:\n' + 
-            '  https://www.instagram.com/firstlymedia)');
+        formPublish.append('message', message);
         formPublish.append('formatting', 'MARKDOWN');
 
         for (let i = 0; i < images.length ; i++) {
