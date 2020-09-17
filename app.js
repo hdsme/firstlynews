74909a93f129dc8acd4b1f0796f77b8efa78c53c
Hao Hoang <38025803+haohoangofficial@users.noreply.github.com>
2020-09-17 11:40:38 +0700

    Update app.js

--- app.js
+++ app.js
@@ -15,19 +15,20 @@ const generator = async () => {
     let random = Math.floor(Math.random() * news.length);
     let feed = news[random];
     let message = feed.description;
+    console.log(message);
     //for (const feed of news) {
         images.push(await createImage.create(feed.img, feed.category, feed.title));
     //console.log(images[0]);
     //    break;
     //}
-
+/*
     let facebook = new Facebook(process.env.ACCESS_TOKEN, process.env.GROUP_ID);
 
     if (process.env.PAGE_ID) {
         facebook.postAsPage(process.env.PAGE_ID)
     }
     facebook.postNews(images,message);
- 
+ */
 };
 
 app.use((req, res, next) => {
