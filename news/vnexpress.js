a34faed2d5979ff5bd903f93a2cfdf949dd34fd1
Hao Hoang <38025803+haohoangofficial@users.noreply.github.com>
2020-09-17 13:36:55 +0700

    Update vnexpress.js

--- news/vnexpress.js
+++ news/vnexpress.js
@@ -101,10 +101,11 @@ function VnExpress() {
             if(rss.link.includes('https://vnexpress.net')) { //not get news from english/photo page
                 let imgLink = getStr(rss.content, '<img src="','"');
                 let title = (rss.content).split('></a></br>')[1];
-                    title = title.trim();
+                    
                     //console.log('1' + title);
                     //console.log('1' + imgLink);
                 if (imgLink && title && category && !imgLink.includes('gif')) {
+                    title = title.trim();
                     //console.log(title);
                     //console.log(imgLink);
                     let response = await fetch(rss.link);
